1a3166051973e30decd7d0c5f9f3b30f27915747
Spandana476 <spandhanaperike@gmail.com>
2025-10-10 09:22:36 +0530

    Add files via upload

---
+++ Area of rectangle.py
@@ -0,0 +1,15 @@
+#Area of rectangle using oops
+class rect:
+    def__init__(self,W,B):
+        self.W=W
+        self.B=B
+        def arearect(self):
+            return self.W*selfB
+
+#driver code
+    W,B=int(input("enter W,B"))
+
+    t=rect(6,5)
+
+    a=t.arearect()
+    print("area of rect",a)
