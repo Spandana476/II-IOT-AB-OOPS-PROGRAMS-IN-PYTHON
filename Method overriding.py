3509cd733dc128fb2b69a9ee9b4ba34d13b15e06
Spandana476 <spandhanaperike@gmail.com>
2025-10-18 09:55:21 +0530

    Add files via upload

---
+++ Method overriding.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def describe(self):
+        print("This is a generic vehicle.")
+
+class Car(Vehicle):
+    
+        print("This is a sporty car.")
+
+class Motorcycle(Vehicle):
+    def discribe(self):
+        print("This is a fast motorcycle.")
+
+vehicle = Vehicle()
+car = Car()
+motorcycle = Motorcycle()
+        
+    
